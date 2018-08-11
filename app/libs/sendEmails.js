d9d02ae82501a60f58d92c486892f063c8b8923a
Mahendra-Singh-Chauhan <m.chauhankkm@gmail.com>
2018-08-11 11:21:03 +0530

    email

--- app/libs/sendEmails.js
+++ app/libs/sendEmails.js
@@ -3,45 +3,37 @@ const nodemailer = require('nodemailer');
 // Generate SMTP service account from ethereal.email
 
 let sendForgotPasswordEmail = (toEmail,code)=>{
-nodemailer.createTestAccount((err, account) => {
-    if (err) {
-        console.error('Failed to create a testing account. ' + err.message);
-        return process.exit(1);
-    }
-
-    console.log('Credentials obtained, sending message...');
-
-    // Create a SMTP transporter object
     let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-       
-        auth: {
-            user: 'zim3f6btkcrawlnx@ethereal.email',
-            pass: 'X9tw8CZMXv9CUeUA13'
-        }
-    });
-
-    // Message object
-    let message = {
-        from: 'msc1994dc@gmail.com',
-        to: `${toEmail}`,
-        subject: 'Forgot password',
-        text: `Hi your secure code is - ${code}`,
-
-    };
-
-    transporter.sendMail(message, (err, info) => {
-        if (err) {
-            console.log('Error occurred. ' + err.message);
-            return process.exit(1);
-        }
-
-        console.log('Email sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Email Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    });
-});
+        service:'gmail',
+          auth: {
+              user: 'msc1994dc@gmail.com',
+              pass: 'msc11dc15'
+          },
+          tls:{
+              rejectUnauthorized:false
+          }
+          
+      });
+  
+      // Message object
+      let message = {
+          from: '"Nodemailer" msc1994dc@gmail.com',
+          to: `${toEmail}`,
+          subject: 'Forgot password',
+          text: `Hi your secure code is - ${code}`,
+  
+      };
+  
+      transporter.sendMail(message, (err, info) => {
+          if (err) {
+              console.log('Error occurred. ' + err.message);
+              return process.exit(1);
+          }
+  
+          console.log('Email sent: %s', info.messageId);
+          // Preview only available when sending through an Ethereal account
+          console.log('Email Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      });
 }
 
 
